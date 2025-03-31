66fb282f9462e7111d1e3ca985f61158d9fd077b
Joshua <Joshua.lucas.rohde@gmail.com>
2025-03-31 10:14:13 -0600

    Added in basic code
    I customized the title and Added in a card set up. I am going to keep moving the styles down.

--- single-screen-a1/my-app/App.js
+++ single-screen-a1/my-app/App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { RFValue } from "react-native-responsive-fontsize";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.body}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Jeremy Renner Food App</Text>
+      </View>
+
+      <View style={styles.recipeCard}>
+        <Image
+          source={{ uri: 'https://placekitten.com/201/200' }}
+          style={styles.imgs}
+        />
+        <View style={{ flex: 1 }}>
+          <Text style={{ fontSize: 18, fontWeight: '600', marginBottom: 4 }}>
+            Mr. Fluffles
+          </Text>
+          <Text style={{ fontSize: 14, color: '#555' }}>
+            CEO of naps. CFO of chaos.
+          </Text>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    margin: 24,
   },
+  body: {
+    flex: 1,
+    padding: 16 
+  },
+  title: {
+    fontSize: RFValue(20) 
+  },
+  recipeCard: {
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    borderRadius: 12,
+    padding: 12,
+    marginBottom: 16,
+    shadowColor: '#000',
+    shadowOpacity: 0.1,
+    shadowRadius: 6,
+    elevation: 3,
+    alignItems: 'center'
+  },
+imgs: { 
+  width: 80, 
+  height: 80, 
+  borderRadius: 8, 
+  marginRight: 12 
+},
+
 });
