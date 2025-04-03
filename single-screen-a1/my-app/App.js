dae2cc05f2fbbf434c1a5e42a70d8097eebfc632
Joshua <Joshua.lucas.rohde@gmail.com>
2025-04-02 21:37:38 -0600

    Added Recipes as well as removed unused things. Cleaned up as well

--- single-screen-a1/my-app/App.js
+++ single-screen-a1/my-app/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import {
   StyleSheet,
   Text,
@@ -13,11 +13,11 @@ import {
   LayoutAnimation,
   Platform,
   UIManager,
-  Alert
+  Alert,
+  ImageBackground
 } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 import { MaterialIcons } from '@expo/vector-icons';
-import { ImageBackground } from 'react-native';
 
 if (Platform.OS === 'android' && UIManager.setLayoutAnimationEnabledExperimental) {
   UIManager.setLayoutAnimationEnabledExperimental(true);
@@ -36,7 +36,7 @@ export default function App() {
       title: 'Almond Milk Chicken Curry',
       author: 'Becky Hardin',
       image: require('./assets/chickencurry.jpg'),
-      quote: '“This curry’s light, creamy, and packed with comfort.”',
+      quote: `“This curry’s light, creamy, and packed with comfort.”\n- Jeremy Renner`,
       time: { prep: '20 mins', cook: '20 mins', total: '40 mins', servings: '4' },
       ingredients: [
         { type: 'title', label: 'Curry Paste' },
@@ -55,30 +55,96 @@ export default function App() {
     },
     {
       id: '2',
-      title: 'Renner Ranch Beans',
-      author: 'Jeremy Renner',
-      image: require('./assets/chickencurry.jpg'),
-      quote: `“A hearty dish for a heroic day.”
+      title: 'Spinach Artichoke Dip with Toasted Pitas',
+      author: 'Angela G',
+      image: require('./assets/spinach-dip.jpg'),
+      quote: `“Five ingredients. Big flavour. This creamy dip’s the kind of thing I’d crush after a long shoot! Warm, cheesy, and gone in minutes.”
 - Jeremy Renner`,
-      time: { prep: '10 mins', cook: '30 mins', total: '40 mins', servings: '6' },
+      time: { prep: '5 mins', cook: '1 hour', total: '1h 5m', servings: '8' },
       ingredients: [
-        { type: 'title', label: 'Beans Section' },
-        { type: 'item', label: '2 cans of beans' },
-        { type: 'item', label: '1 onion, chopped' },
-        { type: 'title', label: 'Seasoning' },
-        { type: 'item', label: 'BBQ sauce' },
-        { type: 'item', label: 'Salt, pepper, paprika' }
+        { type: 'title', label: 'Dip' },
+        { type: 'item', label: '11 oz fresh spinach leaves stems removed' },
+        { type: 'item', label: '8 oz cream cheese' },
+        { type: 'item', label: '½ cup Italian cheese blend (Parmesan, Asiago, Romano)' },
+        { type: 'item', label: '13.75 oz artichoke hearts, drained and chopped' },
+        { type: 'item', label: '.75 oz Garlic & Herb Dressing & Recipe Seasoning Mix' },
+        { type: 'title', label: 'Pitas' },
+        { type: 'item', label: '4 Greek pitas' },
+        { type: 'item', label: '8 tablespoon salted butter' },
+        { type: 'item', label: '1 teaspoon sea salt' },
+        { type: 'item', label: '1 teaspoon Italian seasoning' }
       ],
       instructions: [
-        'Sauté onions',
-        'Add beans and sauce',
+        'Slice pitas using a pizza cutter or cut with kitchen shears into 8 triangle pieces.',
+        'Generously brush both sides or each pita with melted butter, sprinkle with salt and herbs.',
         'Simmer for 30 minutes',
-        'Serve hot',
+        'Bake or air fry at 400º until light golden brown and crispy (about 3-7 minutes).',
+        'Add spinach, cream cheese, Italian cheeses, chopped artichokes and garlic & herb seasoning powder to your crockpot set to high.',
+        'Cook for 1 hour, stirring half way through and more as needed to break up the cream cheese and blend everything together',
+        'The dip is ready when all ingredients have combined together evenly and are piping hot and bubbling. Turn heat down to keep warm so spinach does not overcook but stay warm for serving.'
       ],
     },
+
+    {
+      id: '3',
+      title: 'Grilled Pork Kabobs',
+      author: 'Camille Beckstrand',
+      image: require('./assets/porkkabobs.jpg'),
+      quote: `“Each kabob’s like one of Hawkeye’s arrows. They are precise, powerful, and hits you right in the flavour zone. Juicy, smoky, and straight-up legendary.” 
+– Jeremy Renner`,
+      time: { prep: '4h 20m', cook: '8 mins', total: '1h 28m', servings: '6' },
+      ingredients: [
+        { type: 'item', label: '½ cup red wine vinegar' },
+        { type: 'item', label: '½ cup vegetable oil' },
+        { type: 'item', label: '1 large sweet onion, finely chopped' },
+        { type: 'item', label: '3 Tablespoons garlic cloves, finely chopped' },
+        { type: 'item', label: '2 Tablespoons fresh parsley, finely chopped' },
+        { type: 'item', label: '2 pounds pork tenderloin, (cut into 1 inch cubes)' },
+        { type: 'item', label: 'salt and pepper, to taste' }
+      ],
+      instructions: [
+        'In a gallon-sized resealable bag, mix together red wine vinegar, vegetable oil, onions, garlic and parsley.',
+        'Add pork to bag and marinate in the fridge for at least 4 hours (8-10 hours for maximum flavor).',
+        'If using wooden skewers, let soak in water for 30 minutes. Remove pork from marinade and thread onto skewers, packing them tightly. Generously season pork with salt and pepper.',
+        'Preheat grill to high heat.',
+        'Grill pork for 3-4 minutes on each side or until pork is cooked thoroughly.'
+      ],
+    },
+
+    {
+      id: '4',
+      title: 'Italian Stuffed Peppers',
+      author: 'Erin Clarke',
+      image: require('./assets/peppers.jpg'),
+      quote: `“These stuffed peppers? Clean, sharp, and no-nonsense. Kinda like tracking footprints in a snowstorm. Just like Wind River. cold on the outside, heat underneath.” 
+– Jeremy Renner`,
+      time: { prep: '10 mins', cook: '40 mins', total: '50 mins', servings: '4' },
+      ingredients: [
+        { type: 'item', label: '4 large red bell peppers' },
+        { type: 'item', label: '2 teaspoons extra virgin olive oil' },
+        { type: 'item', label: '1 pound ground chicken or turkey' },
+        { type: 'item', label: '2 teaspoons Italian seasoning' },
+        { type: 'item', label: '½ teaspoon kosher salt' },
+        { type: 'item', label: '¼ teaspoon red pepper flakes or up to ½ teaspoon if you like more spicy' },
+        { type: 'item', label: '1 can no salt added diced tomatoes with juices, 15 ounces' },
+        { type: 'item', label: '1 ½ cups cooked brown rice farro, quinoa, cauliflower rice, or orzo' },
+        { type: 'item', label: '1 cup shredded Mozzarella' },
+        { type: 'item', label: '½ cup Parmesan' },
+        { type: 'item', label: '2 tablespoons chopped fresh basil' },
+
+      ],
+      instructions: [
+        'Preheat your oven to 375 degrees F. Lightly coat a 9×13-inch baking dish with nonstick spray. Slice the bell peppers in half from top to bottom. Remove the seeds and membranes then arrange cut side up in the prepared baking dish.',
+        'Heat the olive oil in a large, nonstick skillet over medium high heat. Add the chicken, Italian seasoning, garlic powder, salt, and red pepper flakes. Cook, breaking apart the meat, until the chicken is browned and cooked through, about 4 minutes. Drain off any excess liquid, then pour in the can of diced tomatoes and their juices. Let simmer for 1 minute.',
+        'Remove the pan from the heat. Stir in the rice (or farro) and ½ cup of the Mozzarella and ¼ cup of the parmesan. Mound the filling inside of the peppers, then top with the remaining cheeses.',
+        'Pour a bit of water into the pan with the peppers—just enough to barely cover the bottom of the pan. Bake uncovered for 30 to 35 minutes, until the peppers are tender and the cheese is melted. Top with fresh basil. Serve hot.'
+      ],
+    },
+
+
   ];
 
-  const toggleSwitch = () => setSwitchValue(previous => !previous);
+  const toggleSwitch = () => setSwitchValue(prev => !prev);
 
   const handleSendMessage = () => {
     if (message.trim()) {
@@ -96,7 +162,7 @@ export default function App() {
 
   const toggleDropdown = (cardId, type) => {
     LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
-    setDropdownStates((prev) => ({
+    setDropdownStates(prev => ({
       ...prev,
       [cardId]: {
         ...prev[cardId],
@@ -106,7 +172,7 @@ export default function App() {
   };
 
   const toggleCheckbox = (cardId, type, index) => {
-    setCheckboxStates((prev) => {
+    setCheckboxStates(prev => {
       const current = prev?.[cardId]?.[type] || {};
       return {
         ...prev,
@@ -156,11 +222,12 @@ export default function App() {
     <ScrollView contentContainerStyle={styles.body}>
       <View style={styles.container}>
         <Text style={styles.title}>Jeremy Renner Food App</Text>
-      </View>
-<View style={styles.darkModeCard}>
+        <View style={styles.darkModeCard}>
           <Text style={styles.darkModeTitle}>Enable Jeremy Mode</Text>
           <Switch value={switchValue} onValueChange={toggleSwitch} />
         </View>
+      </View>
+
       {recipes.map((recipe) => (
         <TouchableWithoutFeedback key={recipe.id} onPress={() => toggleCard(recipe.id)}>
           <View style={styles.recipeCard}>
@@ -218,7 +285,7 @@ export default function App() {
       ))}
 
       <View style={styles.rennerMessageBox}>
-        <Text style={styles.rennerTitle}>Tell Jeremy Renner What You Think Of The Food!</Text>
+        <Text style={styles.rennerTitle}>Tell Jeremy What You Think of the Food!</Text>
         <TextInput
           style={styles.input}
           placeholder="Type your message..."
@@ -253,6 +320,7 @@ const styles = StyleSheet.create({
     margin: 24,
     backgroundColor: '#fff',
     marginTop: 60,
+    borderRadius: 12,
   },
   body: {
     flexGrow: 1,
@@ -262,16 +330,6 @@ const styles = StyleSheet.create({
   title: {
     fontSize: RFValue(18),
     fontWeight: 'bold',
-    
-  },
-  switchRow: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: '80%',
-    marginBottom: 16,
-  },
-  switchLabel: {
-    fontSize: 14,
   },
   recipeCard: {
     backgroundColor: '#fff',
@@ -341,6 +399,7 @@ const styles = StyleSheet.create({
   itemText: {
     fontSize: 13,
     marginLeft: 10,
+    marginRight: 20,
     color: '#333',
   },
   subSectionTitle: {
@@ -395,5 +454,5 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: '500',
     color: '#333',
-  },  
+  },
 });
